3367684b868945786727ef99e1a2fc5cb59aac0c
lizz <elizabeth.ann.soup@gmail.com>
2020-02-10 15:40:45 -0500

    Added favicon

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
 
-  var magic8 = {};
   $("#answer").hide();
 
   //-----Property 1: list of answers
-  magic8.answers = ["All signs point to yes", "All signs point to no", "Seems likely", "Seems unlikely", "It's possible", "Probably not", "The gods favour it", "The stars are not aligned"];
+var answers = ["All signs point to yes", "All signs point to no", "Seems likely", "Seems unlikely", "It's possible", "Probably not", "The gods favour it", "The stars are not aligned"];
   
   //-----Property 2: reveal answer function
-  magic8.revealAnswer = function() {
+ function revealAnswer() {
     $("#8ball").effect("shake");    
 
     setTimeout(function(){
     $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");}, 500);
     
 
-    var randomNumber = Math.floor(Math.random() * this.answers.length);
+    var randomNumber = Math.floor(Math.random() * answers.length);
   
     var answer = this.answers[randomNumber];
     $("#answer").text(answer);
@@ -22,7 +21,7 @@ $(document).ready(function() {
   
   
   //-----Magic 8 ball question click function
-  var question = function() {
+  function question() {
     $("#answer").hide();
     $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
   
